5bb39137199b74440a554aa2c5d8a2cb433ea0ca
devkiraa <devkiraa@gmail.com>
2023-09-18 22:12:56 +0530

    Perfect

--- app.py
+++ app.py
@@ -7,9 +7,10 @@ scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/au
 credentials = ServiceAccountCredentials.from_json_keyfile_name('your-service-account.json', scope)
 gc = gspread.authorize(credentials)
 
-# Open the Google Sheet by its URL
-sheet_url = 'https://docs.google.com/spreadsheets/d/1k1jeAG_hsUA8sni6yu7fYM_fXaPDpRrm4ynWElqEn1A/edit#gid=0'  # Replace with your Google Sheet URL
-worksheet = gc.open_by_url(sheet_url).Sheet1  # Replace 'sheet1' with your actual sheet name
+# Open the Google Sheet by its URL and specify the worksheet by name
+sheet_url = 'https://docs.google.com/spreadsheets/d/1k1jeAG_hsUA8sni6yu7fYM_fXaPDpRrm4ynWElqEn1A/edit?usp=sharing'  # Replace with your Google Sheet URL
+worksheet_name = 'Sheet1'  # Replace with your actual sheet name
+worksheet = gc.open_by_url(sheet_url).worksheet(worksheet_name)
 
 # Open the webcam for QR code scanning
 cap = cv2.VideoCapture(0)
@@ -27,8 +28,12 @@ while True:
         # Data is the QR code content
         print("QR Code Data:", data)
         
-        # Send the QR code data to Google Sheets
-        worksheet.append_row([data])
+        try:
+            # Send the QR code data to Google Sheets
+            worksheet.append_row([data])
+            print("Data sent to Sheets successfully.")
+        except Exception as e:
+            print("Error sending data to Sheets:", str(e))
     
     cv2.imshow('QR Code Scanner', frame)
     
